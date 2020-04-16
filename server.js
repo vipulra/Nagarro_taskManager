38a50dbee5e8d901f16ba44f2ebba3bb772dc292
vipulra <63492410+vipulra@users.noreply.github.com>
2020-04-17 03:20:13 +0530

    Server backend Manager

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { db } = require('./db')
 const todoRoute = require('./routes/todos')
 const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended:  true }))
 app.use(express.json())
 
 app.use('/',express.static(__dirname+'/public'));
